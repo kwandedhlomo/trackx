200b104e8d3cc35301ae4ad6f65b27ad3663bef2
shewe-t <trmshe002@myuct.ac.za>
2025-04-30 13:14:55 +0200

    Adding Annotations page and Newcase Page to the app router

--- trackx-frontend/src/routes/AppRouter.jsx
+++ trackx-frontend/src/routes/AppRouter.jsx
@@ -7,6 +7,8 @@ import SignInPage from "../pages/SignInPage";
 import RegisterPage from "../pages/RegisterPage";
 import OverviewPage from "../pages/OverviewPage";
 import SimulationPage from "../pages/SimulationPage";
+import NewCasePage from "../pages/NewCasePage";
+import AnnotationsPage from "../pages/AnnotationsPage";
 // (Later: import LandingPage from "../pages/LandingPage";
 //         import SignInPage from "../pages/SignInPage";
 //         import ManageCasesPage, etc.)
@@ -23,6 +25,10 @@ function AppRouter() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/overview"element={<OverviewPage />} />
         <Route path="/simulation"element={<SimulationPage />} />
+        <Route path="/new-case" element={<NewCasePage />} />
+        <Route path="/annotations" element={<AnnotationsPage />} />
+
+        {/* Add more routes as needed */}
 
 
         {/* Future routes (once you create the pages) */}
